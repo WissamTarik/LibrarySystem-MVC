ecd77111191e3188e03865566ea3c58f525a2a52
WissamTarik <www.wissamtarik2021@gmail.com>
2025-09-24 21:28:45 +0300

    DAL

---
+++ Library.Company.DAL/Data/Configurations/CategoryConfigurations.cs
@@ -0,0 +1,19 @@
+﻿using Library.Company.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Company.DAL.Data.Configurations
+{
+    public class CategoryConfigurations : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(c => c.Name).HasColumnName("CatName").HasMaxLength(50);
+        }
+    }
+}
---
+++ Library.Company.DAL/Data/Contexts/LibraryDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Library.Company.DAL.Data.Configurations;
+using Library.Company.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Company.DAL.Data.Contexts
+{
+    public class LibraryDbContext:DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BookConfigurations());
+            modelBuilder.ApplyConfiguration(new AuthorConfigurations());
+            modelBuilder.ApplyConfiguration(new CategoryConfigurations());        }
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options):base(options) 
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Author> Authors { get; set; }
+    }
+}
